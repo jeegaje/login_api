c8b19b38e7900feac8f8067ccc219db9d5593d7c
jeegaje <anggajiyanfajar@gmail.com>
2021-08-24 20:55:49 +0700

    updater register

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ from flask_jwt_extended.jwt_manager import JWTManager
 from flask_cors import CORS
 
 from config import Config
-from .routes.login_route import *
+from .routes.api_route import *
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
-app.register_blueprint(loginRoute)
+app.register_blueprint(apiRoute)
 
 jwt = JWTManager(app)
--- app/controller/login_control.py
+++ app/controller/login_control.py
@@ -33,15 +33,3 @@ def user():
             }
         user.append(data)
     return jsonify(user)
-
-def register():
-    #data = request.json
-    firstName = request.json.get('fname')
-    lastName = request.json.get('lname')
-    email = request.json.get('email')
-    password = request.json.get('password')
-    session.add(User(user_email=email, user_password=password, firstName=firstName, lastName=lastName))
-    #session.add(User(**data))
-    session.commit()
-    return jsonify({"massage":"user sudah ditambah"})
-    #return jsonify(data)
