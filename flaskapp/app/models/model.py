25f97d5722b70d39610c0d2a614027b8b2eb4698
jeegaje <anggajiyanfajar@gmail.com>
2021-09-15 08:20:45 +0700

    update kelas, wishlist

--- flaskapp/app/models/model.py
+++ flaskapp/app/models/model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import update
 
 engine = create_engine('mysql+mysqlconnector://admin:admin123@localhost:3306/monggaweb', echo = True)
 Session = sessionmaker(bind=engine)
@@ -23,9 +24,9 @@ class data_diri(Base):
    jenis_kelamin = Column(String)
    tempat_lahir = Column(String)
    tanggal_lahir = Column(Date)
-   agama = Column(String)
    alamat = Column(String)
    nomor_hp = Column(String)
+   update_date = Column(DateTime)
    akun_id = Column(String)
 
 class contact_us(Base):
@@ -38,3 +39,22 @@ class contact_us(Base):
    pesan = Column(String)
    create_date = Column(DateTime)
 
+class kelas(Base):
+   __tablename__ = 'kelas'
+   kelas_id = Column(String, primary_key =  True)
+   nama_kelas = Column(String)
+   harga = Column(String)
+   deskripsi = Column(String)
+   create_date = Column(DateTime)
+   mentor_id = Column(String)
+
+class wishlist(Base):
+   __tablename__ = 'wishlist'
+   wishlist_id = Column(String, primary_key =  True)
+   update_date = Column(DateTime)
+   user_id = Column(String)
+
+class wishlist_kelas(Base):
+   __tablename__ = 'wishlist_kelas'
+   wishlist_id = Column(String, primary_key =  True)
+   kelas_id = Column(String)
